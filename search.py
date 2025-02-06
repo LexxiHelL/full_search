7617d1cb822a03d5df5c4c64f8d9c007e7e09f4a
LexxiHelL <melkayaalya@gmail.com>
2025-02-06 18:32:10 +0300

    Add files via upload

--- search.py
+++ search.py
@@ -1,25 +1,32 @@
 import sys
-from geocode import get_coord, get_ll_span
+from geocode import get_coord
 from viewmap import view_map
+from business import find_business
+from distance import find_distance
 
 
 def main():
     toponym_to_find = ' '.join(sys.argv[1:])
 
-    if toponym_to_find:
-        lat, lon = get_coord(toponym_to_find)
-        ll_spn = f'll={lat},{lon}&spn=0.005,0.005'
-        view_map(ll_spn, 'map')
+    lat, lon = get_coord(toponym_to_find)
+    address_ll = f'{lat},{lon}'
+    span = '0.005,0.005'
+    organization = find_business(address_ll, span, 'аптека')
+    point = organization['geometry']['coordinates']
+    org_lat = float(point[0])
+    org_lon = float(point[1])
+    point_param = f'pt={org_lat},{org_lon},pm2gl'
+    points_param = point_param + f'~{address_ll},pm2rdl'
+    view_map(map_type="map", add_params=points_param)
+    name = organization["properties"]["CompanyMetaData"]["name"]
+    address = organization["properties"]["CompanyMetaData"]["address"]
+    time = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+    distance = round(find_distance((lon, lat), (org_lon, org_lat)))
 
-        ll, spn = get_ll_span(toponym_to_find)
-        ll_spn = f'll={ll}&spn={spn}'
-        view_map(ll_spn, 'map')
-
-        poit_param = f'pt={ll}'
-        view_map(ll_spn, 'map', add_params=poit_param)
-    else:
-        print('No param')
+    snippet = f"Название:\t{name}\nАдрес:\t{address}\nВремя работы:\t{time}\n" \
+              f"Расстояние:\t{distance}м."
+    print(snippet)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
